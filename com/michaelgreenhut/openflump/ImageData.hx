37d0f8e4c838eeb8b5ab79c82938722a410a2e64
JamesMarcil <JamesMarcil@users.noreply.github.com>
2017-05-27 11:55:41 -0400

    Fixing tabs and whitespace.
    Removed all trailing whitespace. Converted all tabs into two space
    characters.

--- com/michaelgreenhut/openflump/ImageData.hx
+++ com/michaelgreenhut/openflump/ImageData.hx
@@ -7,17 +7,17 @@ import flash.geom.Point;
  */
 class ImageData
 {
-	public var location:Point;
-	public var scale:Point;
-	public var texture:String;
-	public var pivot:Point;
+  public var location:Point;
+  public var scale:Point;
+  public var texture:String;
+  public var pivot:Point;
 
-	public function new(texture:String, location:Point, scale:Point, pivot:Point) 
-	{
-		this.location = location;
-		this.scale = scale;
-		this.texture = texture;
-		this.pivot = pivot;
-	}
-	
+  public function new(texture:String, location:Point, scale:Point, pivot:Point)
+  {
+    this.location = location;
+    this.scale = scale;
+    this.texture = texture;
+    this.pivot = pivot;
+  }
+
 }
